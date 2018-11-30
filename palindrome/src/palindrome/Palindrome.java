9e62b6f92f619b82e251d78434a4967060cd26e2
Heritiana <39351839+wrath08@users.noreply.github.com>
2018-11-30 23:30:24 +0200

    initial version

--- palindrome/src/palindrome/Palindrome.java
+++ palindrome/src/palindrome/Palindrome.java
@@ -3,20 +3,42 @@ package palindrome;
 public class Palindrome {
 
 	public static void main(String[] args) {
-		String in = "aaba";
-		System.out.println(palindrome(in));
 	}
 	
-	public static boolean palindrome(String in)
+	public static boolean palindrome(int[] in, int size)
 	{
-		char[] c = in.toCharArray();
-		String reverse = new String();
-		for(int i = in.length()-1; i>=0; i--)
+		int[] reverse = new int[size];
+		int j = 0;
+		for(int i = in.length - 1;  i>=0; i--)
 		{
-			reverse += c[i];
+			reverse[j++] = in[i];
 		}
-		
-		return reverse.equals(in);
+		for(int i = 0; i < in.length; i++)
+		{
+			if(in[i] != reverse[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean palindrome(char[] in, int size)
+	{
+		char[] reverse = new char[size];
+		int j = 0;
+		for(int i = in.length - 1;  i>=0; i--)
+		{
+			reverse[j++] = in[i];
+		}
+		for(int i = 0; i < in.length; i++)
+		{
+			if(in[i] != reverse[i])
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
